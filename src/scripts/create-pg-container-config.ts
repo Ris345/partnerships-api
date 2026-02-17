a131c78c574344de3eb7a04ded4f75b3c1642a1b
Joseph Dvorak <dvorakjt@gmail.com>
2026-02-16 19:08:38 -0800

    updated create-pg-container-config script to create random passwords and added .env.example file

--- src/scripts/create-pg-container-config.ts
+++ src/scripts/create-pg-container-config.ts
@@ -1,19 +1,25 @@
 import fs from 'fs';
 import path from 'path';
+import cryptoRandomString from 'crypto-random-string';
 
 const configFilePath = path.join(
   import.meta.dirname,
   '../../pg-container.config.ts',
 );
 
+const dummyPasswords = {
+  dev: cryptoRandomString({ length: 8, type: 'alphanumeric' }),
+  test: cryptoRandomString({ length: 8, type: 'alphanumeric' }),
+};
+
 const configFileContents = `import { createPGContainerConfig } from './src/scripts/pg-container-config-utils';
  
 export default createPGContainerConfig({
   dev: {
-    dummyPassword: '',
+    dummyPassword: '${dummyPasswords.dev}',
   },
   test: {
-    dummyPassword: '',
+    dummyPassword: '${dummyPasswords.test}',
   },
 });
 `;
