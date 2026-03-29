036d0c701fa5d5c6e091ee0d536177a506c1a77d
Joseph Dvorak <dvorakjt@gmail.com>
2026-03-29 10:59:25 -0700

    updated shape of resolver object

--- src/graphql/resolvers/resolvers.ts
+++ src/graphql/resolvers/resolvers.ts
@@ -4,9 +4,9 @@ import { gqlarr } from '../../model/graphql/generated/types';
 export const resolvers = {
   Query: {
     reward: (
-      _: unknown,
-      __: unknown,
-      ___: unknown,
+      _parent: unknown,
+      _args: unknown,
+      _context: unknown,
       info: GraphQLResolveInfo,
     ) => {
       console.log(JSON.stringify(info, null, 2));
